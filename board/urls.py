07c29b215f42b3fb9c7fef23ade5e7067370146c
eupori <chaemin0919@gmail.com>
2019-12-26 16:21:36 +0900

    댓글 기능 추가

--- board/urls.py
+++ board/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='postList'),
-    path('insert/', views.PostInsert.as_view(), name='insertData'),
+    path('', views.PostList.as_view(), name='post_list'),
+    path('create/', views.PostInsert.as_view(), name='post_create'),
+    path('detail/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    path('edit/<int:pk>/', views.PostEdit.as_view(), name='post_edit'),
+    path('detail/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,39 +1,59 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView, FormView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, FormView, DetailView, UpdateView
 from .models import Post
 from django.contrib.auth.models import User
 from django.utils import timezone
-from .form import PostForm
+from .form import PostForm, CommentForm
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 class PostList(ListView):
-    template_name = 'board/postList.html'
-    context_object_name = 'postList'
-    model = Post
+  template_name = 'board/post_list.html'
+  context_object_name = 'post_list'
+  model = Post
 
-    def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-id')
+  def get_queryset(self):
+    return Post.objects.filter(published_date__lte=timezone.now()).order_by('-id')
 
 
 class PostInsert(CreateView):
-    template_name = 'board/insertData.html'
-    success_url = reverse_lazy('postList')
-    form_class = PostForm
-    model = Post
+  template_name = 'board/post_create.html'
+  success_url = reverse_lazy('post_list')
+  form_class = PostForm
+  model = Post
 
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            print("aaa")
-            return self.form_invalid(form)
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.author = User.objects.get(username='admin')
+    self.object.save()
+    print(self.object)
+    return super().form_valid(form)
 
 
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.author = User.objects.get(username='admin')
-        self.object.save()
-        print(self.object)
-        return super().form_valid(form)
+class PostDetail(DetailView):
+  template_name = 'board/post_detail.html'
+  context_object_name = 'post_detail'
+  model = Post
+
+
+class PostEdit(UpdateView):
+  template_name = 'board/post_edit.html'
+  context_object_name = 'post_edit'
+  success_url = reverse_lazy('post_list')
+  form_class = PostForm
+  model = Post
+
+def add_comment_to_post(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  if request.method == "POST":
+    form = CommentForm(request.POST)
+    if form.is_valid():
+      comment = form.save(commit=False)
+      comment.post = post
+      comment.save()
+      return redirect('post_detail', pk=post.pk)
+  else:
+    form = CommentForm()
+  return render(request, 'board/add_comment_to_post.html', {'form': form})
